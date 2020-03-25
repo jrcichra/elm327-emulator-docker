371e4c06a4527306dcc876b54ebdcd0d62186385
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2020-03-24 23:39:05 -0400

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -18,8 +18,8 @@ docker buildx inspect --bootstrap
 # Phase 2 - sign in
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin 
 # Phase 3 - build a container based on the arg passed in
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t jrcichra/elm327-emulator-:${GITHUB_SHA:0:8} --push .
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t jrcichra/elm327-emulator:${GITHUB_SHA:0:8} --push .
 docker buildx imagetools inspect jrcichra/karmen:${GITHUB_SHA:0:8}
 # Also update :latest (should be quick since we just built)
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t jrcichra/elm327-emulator-:latest --push .
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t jrcichra/elm327-emulator:latest --push .
 docker buildx imagetools inspect jrcichra/karmen:latest
